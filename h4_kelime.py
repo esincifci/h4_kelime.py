d539ba6e1bebe708eb4483d8b02a22e91597737d
unknown <cifciesin0@gmail.com>
2025-03-13 00:28:16 +0300

    kelimelerin karakter sayıları sayıldı

---
+++ h4_kelime.py
@@ -0,0 +1,14 @@
+g='Türkiye nin en büyük teknoloji etkinliği TEKNOFEST,bu yıl İstanbul da düzenleniyor.'
+kelimeler=g.split('')
+
+def donustur(a):
+    sayac=[]
+    for kelime in a:
+        print(kelime)
+        sayici=0
+        for harf in range(len(kelime)):
+            sayici+=1
+            print(sayici)
+            sayac.append(sayici)
+            return sayac 
+        b=donustur(kelimeler)
